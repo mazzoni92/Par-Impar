9c14f39ca1b2a49d04a21bdfd4a7bd117114d4f6
Leandro <ldr_mt@hotmail.com>
2022-10-08 15:56:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite um numero para verificar se é Par ou Impar: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if(numero % 2 == 0){
+    Console.WriteLine($"O numero {numero} é Par");
+}
+else{
+    Console.WriteLine($"O numero {numero} é Impar ");
+}
